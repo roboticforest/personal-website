b0ac93583b589fd5762d5927d2c0d19cc399c757
Robotic Forest <roboticforest@gmail.com>
2024-10-12 00:43:59 -0500

    Init: Run create-react-app and tidy generated content.

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const appContent = <h1>Welcome to my App</h1>
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(appContent);
